6d5533a9063235665e8c38da2428e455c7605541
Waleerat S. Gottlieb <waleerat.gottlieb@icloud.com>
2020-09-23 14:22:54 +0200

    Create sample items

---
+++ TodoList-ISO13/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  TodoList-ISO13
+//
+//  Created by Waleerat Gottlieb on 2020-09-23.
+//
+
+import Foundation
+
+struct K {
+    static let topBarTitle = "My todo List"
+    
+    struct cell {
+        static var todoListItem = "todoListItemCell"
+    }
+}
